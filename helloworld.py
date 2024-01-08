9e390f15071295a51036b699fba5b7bc206cc332
sturdy-cactus <94929470+sturdy-cactus@users.noreply.github.com>
2024-01-08 16:00:06 +0000

    first implementation of metallicity histograms

--- helloworld.py
+++ helloworld.py
@@ -23,10 +23,23 @@ M_ass_array=np.array(M_ass)
 b_y_array=np.array(b_y)
 ageparent_array=np.array(ageparent)
 
+#plt.figure()
+#plt.scatter(b_y_array, M_ass_array, c=ageparent_array, cmap='plasma', s=1)
+#plt.colorbar()
+#plt.gca().invert_yaxis()
+#plt.show()
+
+metallicity_array = np.loadtxt("Nemo_6670.dat", skiprows=1, usecols=(0,))
+
+metallicity_lt_1 = metallicity_array[ageparent_array < 1]
+metallicity_between_1_and_5 = metallicity_array[(ageparent_array > 1) & (ageparent_array < 5)]
+metallicity_gt_5 = metallicity_array[ageparent_array > 5]
+
 plt.figure()
-plt.scatter(b_y_array, M_ass_array, c=ageparent_array, cmap='plasma', s=1)
-plt.colorbar()
-plt.gca().invert_yaxis()
+plt.hist(metallicity_lt_1, bins=np.arange(min(metallicity_lt_1), max(metallicity_lt_1) + 0.1, 0.1).tolist(), alpha=0.5, label='age < 1')
+plt.hist(metallicity_between_1_and_5, bins=np.arange(min(metallicity_between_1_and_5), max(metallicity_between_1_and_5) + 0.1, 0.1).tolist(), alpha=0.5, label='1 < age < 5')
+plt.hist(metallicity_gt_5, bins=np.arange(min(metallicity_gt_5), max(metallicity_gt_5) + 0.1, 0.1).tolist(), alpha=0.5, label='age > 5')
+plt.legend()
 plt.show()
 
 
