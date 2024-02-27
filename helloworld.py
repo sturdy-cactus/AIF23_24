10aac90f9895a345b9d83e7aa26501d5decd05d9
sturdy-cactus <94929470+sturdy-cactus@users.noreply.github.com>
2024-02-27 09:04:02 +0000

    show both plots

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 print("hello world")
 
@@ -23,11 +24,11 @@ M_ass_array=np.array(M_ass)
 b_y_array=np.array(b_y)
 ageparent_array=np.array(ageparent)
 
-#plt.figure()
-#plt.scatter(b_y_array, M_ass_array, c=ageparent_array, cmap='plasma', s=1)
-#plt.colorbar()
-#plt.gca().invert_yaxis()
-#plt.show()
+plt.figure(1)
+plt.scatter(b_y_array, M_ass_array, c=ageparent_array, cmap='plasma', s=1)
+plt.colorbar()
+plt.gca().invert_yaxis()
+plt.savefig("HR diagram.png")
 
 metallicity_array = np.loadtxt("Nemo_6670.dat", skiprows=1, usecols=(0,))
 
@@ -35,7 +36,7 @@ metallicity_lt_1 = metallicity_array[ageparent_array < 1]
 metallicity_between_1_and_5 = metallicity_array[(ageparent_array > 1) & (ageparent_array < 5)]
 metallicity_gt_5 = metallicity_array[ageparent_array > 5]
 
-plt.figure()
+plt.figure(2)
 plt.hist(metallicity_lt_1, bins=np.arange(min(metallicity_lt_1), max(metallicity_lt_1) + 0.1, 0.1).tolist(), alpha=0.5, label='age < 1')
 plt.hist(metallicity_between_1_and_5, bins=np.arange(min(metallicity_between_1_and_5), max(metallicity_between_1_and_5) + 0.1, 0.1).tolist(), alpha=0.5, label='1 < age < 5')
 plt.hist(metallicity_gt_5, bins=np.arange(min(metallicity_gt_5), max(metallicity_gt_5) + 0.1, 0.1).tolist(), alpha=0.5, label='age > 5')
@@ -55,7 +56,4 @@ plt.axvline(x=average_gt_5[1], color='g', linestyle=':', label='med age > 5')
 plt.legend()
 plt.show()
 
-
-
-import sys
 sys.exit()
