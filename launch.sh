475ec3180919c68727fdbb759696cbc9772caeef
sturdy-cactus <94929470+sturdy-cactus@users.noreply.github.com>
2024-02-29 11:03:26 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="https://raw.githubusercontent.com/MilenaValentini/TRM_Dati/main/Nemo_6670.dat"
-OUTPUT_FILE="download.dat"
+OUTPUT_FILE="Nemo_6670.dat"
 
 curl -o "$OUTPUT_FILE" "$URL"
 
