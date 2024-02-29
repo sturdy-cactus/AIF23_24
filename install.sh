176dccd686f19995620f2a025a6cb3f048f74a84
sturdy-cactus <94929470+sturdy-cactus@users.noreply.github.com>
2024-02-29 09:53:09 +0000

    install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mkdir prova_finale_abramo
 
-curl -o prova_finale_abramo/launch.sh https://raw.githubusercontent.com/sturdy-cactus/AIF23_24/master/launch
+curl -o prova_finale_abramo/launch.sh https://raw.githubusercontent.com/sturdy-cactus/AIF23_24/master/launch.sh
 curl -o prova_finale_abramo/prova_finale.py https://raw.githubusercontent.com/sturdy-cactus/AIF23_24/master/prova_finale.py
 
 chmod +x prova_finale_abramo/launch.sh
